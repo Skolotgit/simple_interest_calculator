c9c70a30aead2afb0b22a25d38ee80dd2a004c28
Skolotgit <113667486+Skolotgit@users.noreply.github.com>
2022-09-17 12:15:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+'%';
+}
+
+function compute()
+{   
+    var principal = document.getElementById("principal").value;
+
+    if (principal <= 0 || principal == '') {
+        alert("Enter a positive number")
+        document.getElementById("principal").focus()
+    }
+    else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        var p = document.createElement('p')
+        p.innerHTML = 'If you deposit <mark>' + principal + ',</mark><br>at an interest rate of <mark>' + rate + '%.</mark><br>You will receive an amount of <mark>' + interest + ',</mark><br>in the year ' + year;
+        document.getElementById("result").append(p)
+    }
+    
+    
+}
+        
